3965efa349383d941fda946e2b93af817240d910
Xiaokang129 <xiaokang129@outlook.com>
2022-06-01 09:30:41 +0800

    apply changes

--- py2.py
+++ py2.py
@@ -1,15 +1,20 @@
-def kaisa(s):  
-    if 'a'<=s<'x' or 'A'<=s<'X':  
-        ms=chr(ord(s)+3)  
-    elif 'x'<=s<='z' or 'X'<=s<='Z':  
-        ms=chr(ord(s)-23)  
-    return ms  
+ml = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-s = raw_input()
-l = ""
-for i in range(0, len(s)):
-    if s[i].isalpha():
-        l += kaisa(s[i])
-    else:
-        l += s[i]
-print(l)
+def isleap(a):    
+    if (a%400 == 0): return True    
+    elif (a%4 == 0):      
+        if (a%100 == 0): return False  
+        else: return True  
+    else: return False 
+
+def day(y, m, d):
+    if (isleap(y)): ml[1] = 29
+    c = 0
+    for mf in range(0, m):
+        for df in range(1, ml[mf]+1):
+            c += 1
+            if (mf == m - 1 and df == d): break
+    return c
+
+y,m1,d1,m2,d2=map(int,raw_input().split())
+print day(y,m2,d2)-day(y,m1,d1)  
