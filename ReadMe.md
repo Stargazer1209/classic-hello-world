ab2dbb0ef0d6cea028d158b4cae70a5ad4809e58
Xiaokang129 <xiaokang129@outlook.com>
2023-03-06 15:56:13 +0800

    readme added

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Hello world environment test for serveral languages.
--- py2.py
+++ py2.py
@@ -1,20 +1,20 @@
-ml = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-def isleap(a):    
-    if (a%400 == 0): return True    
-    elif (a%4 == 0):      
-        if (a%100 == 0): return False  
-        else: return True  
-    else: return False 
-
-def day(y, m, d):
-    if (isleap(y)): ml[1] = 29
-    c = 0
-    for mf in range(0, m):
-        for df in range(1, ml[mf]+1):
-            c += 1
-            if (mf == m - 1 and df == d): break
-    return c
-
-y,m1,d1,m2,d2=map(int,raw_input().split())
-print day(y,m2,d2)-day(y,m1,d1)  
+def isa(s):
+    if (s[0] == s[3] and s[1] == s[2] and s[0] == s[1]):
+        return True
+    else:
+        return False
+s = raw_input()
+t = s.split(" ")
+t = sorted(t)
+print(t)
+a = 0
+f = -1
+m = "10000"
+for i in range(0, len(t)):
+    if (t[i] < m):
+        m = t[i]
+        f = i
+        if (isa(t[i])):
+            a = 1
+if (a == 0):
+    print ()
